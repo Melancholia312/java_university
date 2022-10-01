6dfeb73cf36e3ab57eaa89cb2a4d6731b4bc08a2
Melancholia312 <67428510+Melancholia312@users.noreply.github.com>
2022-10-02 02:35:37 +0300

    Update City.java

--- practice/1.3/City.java
+++ practice/1.3/City.java
@@ -1,20 +1,34 @@
 import java.util.HashMap;
+import java.util.Map;
 
 public class City {
 
-    private HashMap<String, Integer> ways;
+    private HashMap<City, Integer> ways = new HashMap<>();
     private String name;
 
     public City(String name) {
         this.name = name;
     }
 
-    public City(String name, HashMap<String, Integer> ways) {
+    public City(String name, HashMap<City, Integer> ways) {
         this(name);
         this.ways = ways;
     }
 
+    public void addOneDirectionWay(City city, int wayValue){
+        ways.put(city, wayValue);
+    }
+
+    public void addTwoDirectionWay(City city, int wayValue){
+        ways.put(city, wayValue);
+        city.addOneDirectionWay(this, wayValue);
+    }
+
     public String toString() {
-        return String.format("%s: %s", name, ways);
+        String stringCity = "";
+        for (Map.Entry<City, Integer> entry : ways.entrySet()) {
+            stringCity += String.format("%s : %d; ", entry.getKey().name, entry.getValue());
+        }
+        return String.format("%s - {%s}", name, stringCity.trim());
     }
 }
