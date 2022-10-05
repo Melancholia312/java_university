d58553b99c7475e3c1e18ef3f0c8473bae1a3e92
Melancholia312 <67428510+Melancholia312@users.noreply.github.com>
2022-10-05 04:07:55 +0300

    Update Name.java

--- practice/1.4/Name.java
+++ practice/1.4/Name.java
@@ -16,6 +16,30 @@ public class Name {
         this.surname = (surname == null) ? "" : surname;
         this.patronymic = (patronymic == null) ? "" : patronymic;
     }
+    
+        public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
 
     public String toString() {
         return String.format("%s %s %s", name, surname, patronymic).trim();
