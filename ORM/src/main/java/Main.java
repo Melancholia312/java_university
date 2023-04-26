0c46e6090d9754720a0e822e3267804aa1ec2198
Melancholia312 <67428510+Melancholia312@users.noreply.github.com>
2023-04-27 00:36:38 +0400

    Update Main.java

--- ORM/src/main/java/Main.java
+++ ORM/src/main/java/Main.java
@@ -1,14 +1,13 @@
 import java.lang.reflect.InvocationTargetException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Main {
 
     public static void main(String[] args) throws SQLException, InvocationTargetException, InstantiationException, IllegalAccessException, NoSuchMethodException {
-        System.out.println(ORM.selectAll(Employee.class));
+        ORM orm = new ORM();
+        System.out.println(orm.selectAll(Employee.class));
         //Connected to the PostgreSQL server successfully.
         //[Employee{id=1, name='Vasya', id_dep=1}, Employee{id=2, name='Sveta', id_dep=1}, Employee{id=3, name='Vitya', id_dep=1}]
     }
 }
+
