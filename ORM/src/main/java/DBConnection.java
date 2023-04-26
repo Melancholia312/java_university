830e494d9233a8531652b230a068efc8cc974152
Melancholia312 <67428510+Melancholia312@users.noreply.github.com>
2023-04-26 22:24:02 +0400

    Add files via upload

---
+++ ORM/src/main/java/DBConnection.java
@@ -0,0 +1,24 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    private final String url = "jdbc:postgresql://localhost/workshop_db";
+    private final String user = "postgres";
+    private final String password = "TombRaider2003";
+
+    public Connection connect() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Connected to the PostgreSQL server successfully.");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return conn;
+    }
+}
+
